b59c27e67f89ca6e6f9701ce242bc93a90f68860
weizhanzhan <375786117@qq.com>
2019-01-13 21:27:17 +0800

    优化layout

---
+++ docs/.vuepress/components/Common/Democode.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        demo-code
+    </div>
+</template>
+
+<script>
+export default {
+    name:'demo-code'
+}
+</script>
+
+<style>
+
+</style>
---
+++ docs/.vuepress/components/src/test/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        我是test组建
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
