488fdbb09ecbf747c66cd086a4df5e091757e000
weizhanzhan <375786117@qq.com>
2019-01-14 21:14:41 +0800

    懒加载组建

--- examples/router.js
+++ examples/router.js
@@ -7,27 +7,27 @@ const router = new VueRouter({
     routes:[
         {
             path:'/',
-            component:()=>import('./views/home/index.vue')
+            component: r => require.ensure([], () => r(require('./views/home/index.vue')))
         },
         {
             path:'/components',
             name:'layout',
-            component:()=>import('./views/layout/index.vue'),
+            component: r => require.ensure([],() => r(require('./views/layout/index.vue'))),
             children:[
                 {
                     path:'install',
                     name:'install',
-                    component:()=>import('./views/demo/install')
+                    component: r => require.ensure([],() => r(require('./views/demo/install')))
                 },
                 {
                     path:'button',
                     name:'button',
-                    component:()=>import('./views/demo/button')
+                    component:r => require.ensure([],() => r(require('./views/demo/button')))
                 },
                 {
                     path:'icon',
                     name:'icon',
-                    component:()=>import('./views/demo/wz-icon/index.md')
+                    component:r => require.ensure([],() => r(require('./views/demo/wz-icon/index.md')))
                 }
             ]
         }
