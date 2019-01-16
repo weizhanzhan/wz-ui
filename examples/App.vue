c746ea7e12c8d771c46d22a1fa46c119e8851e2c
weizhanzhan <375786117@qq.com>
2019-01-16 19:19:41 +0800

    中引文切换

--- examples/App.vue
+++ examples/App.vue
@@ -1,7 +1,5 @@
 <template>
   <div id="app">
-    {{lang}}
-    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <router-view></router-view>
   </div>
 </template>
--- examples/views/layout/header/index.vue
+++ examples/views/layout/header/index.vue
@@ -7,6 +7,10 @@
             
             <ul class="wz-header-nav">
                 <li> <input type="text" placeholder="Search.."></li>
+                <li> 
+                    <i  class="iconfont icon-zh-CN" v-if="app.lang=='zh-CN'" @click="app.changeLang('en-US')" ></i>
+                    <i  class="iconfont icon-en-US" v-else  @click="app.changeLang('zh-CN')"></i>
+                </li>
                 <li> <i  class="iconfont icon-git-1"></i></li>
             </ul>
         </div>
@@ -15,7 +19,9 @@
 </template>
 
 <script>
-
+    export default {
+        inject:['app']
+    }
 </script>
 
 <style>
